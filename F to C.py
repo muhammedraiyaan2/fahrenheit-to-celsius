07cc57f897d69c4494c5d5c7d34688fa7817f136
Muhammad Raiyaan <83336994+muhammedraiyaan2@users.noreply.github.com>
2021-06-22 17:33:29 +0530

    Add files via upload

---
+++ F to C.py
@@ -0,0 +1,6 @@
+print("Hi welcome to the farenheat to celius coventer")
+#Here I printed some message
+ff=int(input("Enter the fareheat value: "))
+#Here I will take input from the user of f value
+print((ff-32)*5/9)
+#And I finally added the formula and printed the value
